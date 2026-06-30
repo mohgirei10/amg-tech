1ce1dee3ba2586e4faf1e3f678e74548884afe90
unknown <babsgirei@example.com>
2026-06-30 21:19:08 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // Optional: If you ever switch to the Next.js <Image> component, 
+  // you will need to disable its built-in server-side optimization for GitHub Pages:
+  // images: { unoptimized: true }
 };
 
 export default nextConfig;
